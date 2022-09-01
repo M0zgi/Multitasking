48ceb23625b586e3a021108da426d09afddc6d65
Aleksandr <mozg.com@gmail.com>
2022-09-01 14:47:45 +0300

    add task3

---
+++ HW1/Task3/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+
+namespace Task1
+{
+    class Program
+    {
+        
+
+        static void Main()
+        {
+            
+            
+        }
+    }
+}
